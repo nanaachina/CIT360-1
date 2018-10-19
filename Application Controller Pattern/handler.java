562ed32f52667726cdf9f3c1943308804054571d
seth9341 <seth9341@gmail.com>
2018-10-19 13:28:36 -0500

    added handler java

---
+++ Application Controller Pattern/handler.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ACDemo;
+
+/**
+ *
+ * @author mpw33
+ */
+
+
+import java.util.HashMap;
+
+public interface Handler {
+    public void handleIt(HashMap<String, Object> data);
+}
