d6a19b09ce9cac4f94c46bf66d5b8b2674edf708
seth9341 <seth9341@gmail.com>
2019-01-26 16:55:14 -0600

    amended javacollections

--- Java Collections/JavaCollections/src/javacollections/JavaCollections.java
+++ Java Collections/JavaCollections/src/javacollections/JavaCollections.java
@@ -13,9 +13,6 @@ import java.util.*;
 
 public class JavaCollections {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         
