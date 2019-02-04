0b8a1e585176e34d4e0e49160c0682946dcfa31d
Seth Huntley <seth9341@gmail.com>
2019-02-04 13:47:22 -0600

    made very simple runnable example using the doing more with java examples

--- Threads, Executors, and Runnables/ThreadsExecutorsRunnables/src/threadsexecutorsrunnables/RunnableExample.java
+++ Threads, Executors, and Runnables/ThreadsExecutorsRunnables/src/threadsexecutorsrunnables/RunnableExample.java
@@ -11,17 +11,33 @@ package threadsexecutorsrunnables;
  */
 public class RunnableExample implements Runnable {
 
-        @Override
-	public void run(){
-		for (int i = 0; i < 3; i++) {
-			System.out.println("Thread id: "
-                +Thread.currentThread().getName());
-			try {
-				Thread.sleep(100);
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+          public static String runnableMaybe;
+    public void test(){
+        for(int grade=1;grade<=10;grade++){
+            System.out.println(Thread.currentThread().getName()+" got number "+ grade +" right ");
+            boolean ifPassed = this.ifPassed(grade);
+            if(ifPassed){
+                break;
+            }
+        }
+    }
+    private boolean ifPassed(int pass){
+        boolean ifPassed = false;
+        if((RunnableExample.runnableMaybe==null)&&(pass==10)){
+            String passName=Thread.currentThread().getName();
+            RunnableExample.runnableMaybe=passName;
+            System.out.println("You passed!");
+            ifPassed=true;
+        }
+        else if(RunnableExample.runnableMaybe==null){
+            ifPassed=false;
+        }
+        else if(RunnableExample.runnableMaybe==null){
+            ifPassed=true;
+        }
+        return ifPassed;
+    }
+    public void run(){
+        this.test();
+    }
 }
